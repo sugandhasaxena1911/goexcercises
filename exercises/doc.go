8a497e2cd3fe6357a38570c837bd8d968d1b622e
sugandhasaxena1911 <sugandhasaxena1911@gmail.com>
2023-04-04 16:52:04 +0530

    document

---
+++ exercises/doc.go
@@ -0,0 +1,34 @@
+/*
+hello thsi is a comment 
+i am a document . 
+very huge hello thsi is a comment 
+i am a document . 
+very huge hello thsi is a comment 
+i am a document . 
+very huge hello thsi is a comment 
+i am a document . 
+very huge hello thsi is a comment 
+i am a document . 
+very huge hello thsi is a comment 
+i am a document . 
+very huge hello thsi is a comment 
+i am a document . 
+very huge hello thsi is a comment 
+i am a document . 
+very huge hello thsi is a comment 
+i am a document . 
+very huge hello thsi is a comment 
+i am a document . 
+very huge hello thsi is a comment 
+i am a document . 
+very huge hello thsi is a comment 
+i am a document . 
+very huge hello thsi is a comment 
+i am a document . 
+very huge hello thsi is a comment 
+i am a document . 
+very huge hello thsi is a comment 
+i am a document . 
+very huge 
+*/
+package exercises
